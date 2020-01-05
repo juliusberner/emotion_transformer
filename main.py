432472447bfcf2f5a52b8c4d2d8908093e9b4b77
Julius <j.berner.igg@web.de>
2020-01-06 00:10:09 +0100

    release

---
+++ main.py
@@ -0,0 +1,14 @@
+
+from emotion_detect.core import EmotionModel, get_args, main
+
+if __name__ == '__main__':
+    hparams = get_args(EmotionModel)
+    hparams = hparams.parse_args()
+
+    if hparams.mode == 'default':
+        main(hparams)
+    elif hparams.mode == 'hparams_search':
+        if hparams.gpus == 0:
+            hparams.optimize_parallel_cpu(main, nb_trials=20, nb_workers=1)
+        else:
+            hparams.optimize_parallel_gpu(main, nb_trials=20, gpus = list(range(hparams.gpus)))
